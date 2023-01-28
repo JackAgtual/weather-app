11f7d5b0f940e421e514f7d83f002cd543ca33f4
Jack Agtual <jaagtual@gmail.com>
2023-01-27 21:55:26 -0800

    Adding form validation

--- src/weatherController.js
+++ src/weatherController.js
@@ -4,20 +4,27 @@ export default function WeatherController(FetchWeather, UnitConverter) {
     const _cityValueEl = document.getElementById('city')
     const _tempValueEl = document.getElementById('temperature')
 
+
+
     const presentWeatherData = async e => {
         e.preventDefault()
 
+        _input.setCustomValidity('')
         try {
             const data = await FetchWeather.getWeatherAtLocation(_input.value)
             console.log(`data: ${data}`)
             _cityValueEl.innerText = data.name
             _tempValueEl.innerText = `${Math.round(UnitConverter.kelvin2fahrenhet(data.main.temp))} \u00B0F`
         } catch {
-            console.log(`invalid city`)
             _input.setCustomValidity('Invalid city')
+            _input.reportValidity()
+            _input.focus()
+            _input.select()
         }
     }
 
     _form.addEventListener('submit', presentWeatherData)
 
+    // prevent custom validity message from popping up
+    _input.addEventListener('input', () => _input.setCustomValidity(''))
 }
