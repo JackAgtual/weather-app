18e610efef8a4389efb6198e99afb7a205f26a1b
Jack Agtual <jaagtual@gmail.com>
2023-01-27 22:12:22 -0800

    Using functions for rendering valid and invalid search

--- src/weatherController.js
+++ src/weatherController.js
@@ -4,22 +4,28 @@ export default function WeatherController(FetchWeather, UnitConverter) {
     const _cityValueEl = document.getElementById('city')
     const _tempValueEl = document.getElementById('temperature')
 
+    const _renderPageForInvalidSearch = () => {
+        _input.setCustomValidity('Invalid city')
+        _input.reportValidity()
+        _input.focus()
+        _input.select()
+    }
 
+    const _renderPageForValidSearch = async () => {
+        const data = await FetchWeather.getWeatherAtLocation(_input.value)
+        _cityValueEl.innerText = data.name
+        _tempValueEl.innerText = `${Math.round(UnitConverter.kelvin2fahrenhet(data.main.temp))} \u00B0F`
+        _input.blur()
+    }
 
-    const presentWeatherData = async e => {
+    const presentWeatherData = e => {
         e.preventDefault()
 
         _input.setCustomValidity('')
         try {
-            const data = await FetchWeather.getWeatherAtLocation(_input.value)
-            console.log(`data: ${data}`)
-            _cityValueEl.innerText = data.name
-            _tempValueEl.innerText = `${Math.round(UnitConverter.kelvin2fahrenhet(data.main.temp))} \u00B0F`
+            _renderPageForValidSearch()
         } catch {
-            _input.setCustomValidity('Invalid city')
-            _input.reportValidity()
-            _input.focus()
-            _input.select()
+            _renderPageForInvalidSearch()
         }
     }
 
