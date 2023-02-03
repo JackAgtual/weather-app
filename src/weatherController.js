bc21e9455f6aae6b44a4c3aa5321d74b5dacc4a2
Jack Agtual <jaagtual@gmail.com>
2023-02-02 18:20:43 -0800

    Temperature unit change is working; needs to be refactored

--- src/weatherController.js
+++ src/weatherController.js
@@ -9,6 +9,12 @@ export default function WeatherController(FetchWeather, UnitConverter) {
     const _humidityValueEl = document.getElementById('humidity')
     const _windspeedValueEl = document.getElementById('windspeed')
 
+    let _currentUnitIsF = true
+    let _temperatureVal = undefined
+    let _feelsLikeVal = undefined
+    const _unitSelectorF = document.getElementById('unit-f')
+    const _unitSelectorC = document.getElementById('unit-c')
+
     const _renderPageForInvalidSearch = () => {
         _input.setCustomValidity('Invalid city')
         _input.reportValidity()
@@ -31,6 +37,9 @@ export default function WeatherController(FetchWeather, UnitConverter) {
 
     const _renderPageForValidSearch = async cityName => {
         const data = await FetchWeather.getWeatherAtLocation(cityName)
+
+        _storeTemperatureValues(data)
+
         _tempValueEl.innerText = `${Math.round(data.main.temp)}`
         _weatherIcon.src = `http://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`
         _locationValueEl.innerText = `${data.name}, ${data.sys.country}`
@@ -51,6 +60,36 @@ export default function WeatherController(FetchWeather, UnitConverter) {
             _renderPageForInvalidSearch()
         }
     }
+
+    const _storeTemperatureValues = data => {
+        // store temperatures for unit conversions
+        _temperatureVal = data.main.temp
+        _feelsLikeVal = data.main.feels_like
+    }
+
+    const _toggleSelectedUnitClassOnUnitElements = () => {
+        _unitSelectorC.classList.toggle('unselected-unit')
+        _unitSelectorF.classList.toggle('unselected-unit')
+    }
+
+    const toggleTemperatureUnits = () => {
+        if (_currentUnitIsF) {
+            _temperatureVal = UnitConverter.fahrenheit2celcius(_temperatureVal)
+            _feelsLikeVal = UnitConverter.fahrenheit2celcius(_feelsLikeVal)
+        } else {
+            _temperatureVal = UnitConverter.celcius2fahrenheit(_temperatureVal)
+            _feelsLikeVal = UnitConverter.celcius2fahrenheit(_feelsLikeVal)
+        }
+
+        _tempValueEl.innerText = `${Math.round(_temperatureVal)}`
+        _feelsLikeTempValueEl.innerText = `Feels like ${Math.round(_feelsLikeVal)}`
+
+        _toggleSelectedUnitClassOnUnitElements()
+        _currentUnitIsF = !_currentUnitIsF
+    }
+
+    _unitSelectorC.addEventListener('click', toggleTemperatureUnits)
+    _unitSelectorF.addEventListener('click', toggleTemperatureUnits)
 
     _form.addEventListener('submit', presentWeatherData)
 
